9da52d31b5dceb10ba5bd6041ce6b33ca7730a0a
lidingisboss <1120099539@qq.com>
2018-04-02 08:15:16 +0800

    first text

---
+++ JDtext/app/js/money/mslider.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+    <Panel title="轮播图" :class="$style.panel">
+        <section :class="$style.content">
+            <Slider :items="items" :cname="$style.slider"/>
+        </section>
+    </Panel>
+</template>
+
+<script>
+import Panel from "../core/panel.vue"
+import Slider from "../core/slider.vue"
+export default {
+    components: {
+        Panel,
+        Slider,
+    },
+    data() {
+        return {
+            items: [{
+                href: "home",
+                src: "//img12.360buyimg.com/jrpmobile/jfs/t18787/96/583735472/19041/545127d6/5a976b05N1a6a412a.jpg?width=750&height=400",
+            }, {
+                href: "home",
+                src: "//img12.360buyimg.com/jrpmobile/jfs/t18724/53/552742550/28349/141d0cb1/5a976ab5N1f131e9c.jpg?width=750&height=400",
+            },{
+                href: "home",
+                src: "//img12.360buyimg.com/jrpmobile/jfs/t18787/96/583735472/19041/545127d6/5a976b05N1a6a412a.jpg?width=750&height=400",
+            }],
+        }
+    },
+}
+</script>
+
+<style lang="scss" module>
+  @import "../../css/element.scss";
+  .panel{
+    @include panel;
+    >h4{
+      display: none;
+    }
+    .content{
+      .slider{
+        margin-top: 120px;
+        img{
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
